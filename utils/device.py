2fc8460a9c46d1fdd60bc0e3e2bfc4552d4d6115
ielym <ieluoyiming@163.com>
2022-01-13 19:31:00 +0800

    device

--- utils/device.py
+++ utils/device.py
@@ -1,21 +1,28 @@
 import os
 import torch
 
-def set_device(device_str):
-    device_str = device_str.replace(' ', '')
+def set_device(devices):
 
-    if device_str == 'cpu':
+    if len(devices) == 0:
         os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
     else:
+        device_str = ','.join(devices)
         os.environ['CUDA_VISIBLE_DEVICES'] = device_str
 
-    cuda = device_str != 'cpu' and torch.cuda.is_available()
+        shards = {}
+        for shard_idx, device in enumerate(devices):
+            shards[shard_idx] = device
+        os.environ['FASTVISON_SHARDS'] = device_str
+
+
+    cuda = len(devices) and torch.cuda.is_available()
 
     device = torch.device('cuda' if cuda else 'cpu')
 
-    device_msg = f'Device : {device.type}'
+    device_msg = f'Device : {device.type} \t'
     if device.type == 'cuda':
-        device_msg += f"CUDA_VISIBLE_DEVICES : {os.environ['CUDA_VISIBLE_DEVICES']}"
+        device_msg += f"CUDA_VISIBLE_DEVICES : {os.environ['CUDA_VISIBLE_DEVICES']}\t"
+        device_msg += f"shards : { os.environ['FASTVISON_SHARDS']}"
     print(device_msg)
 
     return device
